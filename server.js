243f6f6b22aaa844eaabc6cbea74f31ff4d3b55f
pallavimahajan11 <84033853+pallavimahajan11@users.noreply.github.com>
2023-03-31 12:16:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,15 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import {fileURLToPath} from 'url';
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -28,7 +32,7 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.use("*", function (req, res) {
+app.use("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
